006037608a3c7c29c07ab5b697d5d8e4690e064a
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-03-18 12:07:50 -0400

    fix debug in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,22 +35,13 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create(req.body)
-  .then((category) => {
-    if (req.body.Id.length) {
-      const categoryIdArr = req.body.Id.map((category_id) => {
-        return {
-          category_id: category.id,
-        };
-      });
-      return Category.bulkCreate(categoryIdArr);
-    }
-    res.status(200).json(category);
+  Category.create({
+    category_name: req.body.category_name
   })
-  .then((categoryIds) => res.status(200).json(categoryIds))
-  .catch((err) => {
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
     console.log(err);
-    res.status(400).json(err);
+    res.status(500).json(err);
   });
 });
 
@@ -58,66 +49,40 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
   Category.update(req.body, {
     where: {
-      id: req.params.id,
-    },
+        id: req.params.id
+    }
   })
-  .then((updatedCategory) => {
-    return Category.findAll({
-    });
+    .then(categoryData => {
+        if (!categoryData[0]) {
+            res.status(404).json({ message: 'No category found with this id'});
+            return;
+        }
+        res.json(categoryData);
   })
-  .then((category) => {
-    const categoryIds = category.map(({category_id}) => category_id);
-
-    const newCategory = req.body.categoryIds
-    .filter((category_id) => !categoryIds.include(category_id))
-    .map((category_id) => {
-      return {
-        category_id,
-      };
-    });
-
-    const categoryToRemove = category
-    .filter(({category_id}) => !req.body.categoryIds.includes(category_id))
-    .map(({id}) => id); 
-
-    return Promise.all([
-      Category.destroy({ where: {id: categoryToRemove}}),
-      Category.bulkCreate(newCategory),
-    ]);
-  })
-  .then((updatedCategory) => res.json(updatedCategory))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err);
   });
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   Category.destroy({
     where: {
-      id: req.params.id
+        id: req.params.id
     }
   })
-  .then((category) => {
-    return Category.findAll({
-      where: {
-        id: req.params.id
-      }
-    });
+    .then(categoryData => {
+        if (!categoryData) {
+            res.status(404).json({ message: 'No category found with this id'});
+            return;
+        }
+        res.json(categoryData);
   })
-  .then((category) => {
-    const categoryIds = category.map(({id}) => id);
-    return Category.destroy({
-      where: {
-        id: categoryIds
-      }
-    });
-  })
-  .then((deletedCategory) => res.json(deletedCategory))
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
   });
 });
 
